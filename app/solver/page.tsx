cbae1e6c1fc28f17413a7858cfd549ec09994172
Hassan Amir <hassanamirorg@gmail.com>
2025-07-03 10:25:15 +0500

    Layout fix for solver page. (Left Margin)

--- app/solver/page.tsx
+++ app/solver/page.tsx
@@ -124,7 +124,7 @@ const Solver = () => {
   return (
     <div className="w-full h-screen grid place-items-center">
       <Toaster richColors expand position="bottom-right"></Toaster>
-      <div className={"solverPage opacity-0 ml-37.75"}>
+      <div className={"solverPage opacity-0"}>
         <h1 className="flex flex-row content-center mb-5.5 h-10 w-189 font-sans bg-secondary rounded text-[1.05rem]">
           <Image
             src={"/info-light.svg"}
